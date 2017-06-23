9b9f9763ed8ab35a98fc8dfc6ae70973c10cf09b
Zach Woodward <zachw38@gmail.com>
2017-06-22 22:55:54 -0400

    some stuff with word lengths, but got some way to go

--- articleanalysis/start.py
+++ articleanalysis/start.py
@@ -11,14 +11,30 @@ class FileReader(object):
     def page_to_string(self, pagenum=0):
         return self.pdf.pages[pagenum].extractText()
 
-    def create_word_dict(self):
+    def create_word_list(self):
         page0 = self.page_to_string()
         self.words = page0.split()
+
+    def create_word_length_dict(self):
+        self.word_lens = {}
+        for word in self.words:
+            self.word_lens[word] = len(word)
 
 
 if __name__ == '__main__':
     reader = FileReader()
+    reader.create_word_list()
 
-    for pagenum in range(0, reader.pdf.getNumPages() / 4):
-        print "Page %d" % pagenum
-        print reader.page_to_string(pagenum)
+    print len(reader.words)
+
+    for i in range(100, 200):
+        print reader.words[i],
+
+    reader.create_word_length_dict()
+    
+    i = 0
+    for word, length in reader.word_lens.items():
+        print word, length
+        i += 1
+        if i == 14:
+            break;
--- tests/articleanalysis_tests.py
+++ tests/articleanalysis_tests.py
@@ -1,6 +1,11 @@
 from nose.tools import *
 from articleanalysis.start import FileReader
 
-def test_basic():
+def test_reader_pdf_is_useful():
     reader = FileReader()
     assert reader.pdf.getNumPages() > 0
+
+def test_reader_has_words():
+    reader = FileReader()
+    reader.create_word_list()
+    assert len(reader.words) > 0
