6efe953d41c7ee6679acf6d4db8e3a318e3b68f0
Zach Woodward <zachw38@gmail.com>
2017-06-27 23:29:03 -0400

    unit tests make more sense now

--- tests/articleanalysis_tests.py
+++ tests/articleanalysis_tests.py
@@ -1,11 +1,27 @@
 from nose.tools import *
-from articleanalysis.start import FileReader
+from articleanalysis.PdfTextMining import PdfTextMining
 
-def test_reader_pdf_is_useful():
-    reader = FileReader()
-    assert reader.pdf.getNumPages() > 0
+testpage = "this is a test string for testing a complicated process"
+words = testpage.split()
 
-def test_reader_has_words():
-    reader = FileReader()
-    reader.create_word_list()
-    assert len(reader.words) > 0
+def test_mining_creates_array():
+    reader = PdfTextMining(words)
+    assert len(reader.word_lens.items()) > 0
+
+def test_biggest_word():
+    reader = PdfTextMining(words)
+    biggest_word, bw_length = reader.biggest_word()
+    assert biggest_word == "complicated"
+    assert bw_length == 11
+
+def test_smallest_word():
+    reader = PdfTextMining(words)
+    smallest_word, sw_length = reader.smallest_word()
+    assert smallest_word == "a"
+    assert sw_length == 1
+
+def test_biggest_gt_smallest():
+    reader = PdfTextMining(words)
+    biggest_word, bw_length = reader.biggest_word()
+    smallest_word, sw_length = reader.smallest_word()
+    assert bw_length > sw_length
